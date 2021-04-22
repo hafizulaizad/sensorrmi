b38c6bd5d512b3a14d1876e83662974269f9a8ed
Hafizul Aizad <hafizulm.noor@gmail.com>
2021-04-22 21:25:12 +0800

    Commit update

--- sensorrmi/src/TemperatureSensorManager.java
+++ sensorrmi/src/TemperatureSensorManager.java
@@ -42,15 +42,15 @@ public class TemperatureSensorManager extends UnicastRemoteObject implements Tem
 
 	@Override
 	public float getAverageTemperature() throws RemoteException {
-		int d1 = temp.get("Monday");
-		int d2 = temp.get("Tuesday");
-		int d3 = temp.get("Wednesday");
-		int d4 = temp.get("Thursday");
-		int d5 = temp.get("Friday");
-		int d6 = temp.get("Saturday");
-		int d7 = temp.get("Sunday");
+		HashMap<String,Integer>data = getTemperatureDays();
 		
-		float average = (d1+d2+d3+d4+d5+d6+d7)/7;
+		float total = 0;
+		
+		for(int temperature:data.values()) {
+			total += temperature;
+		}
+		
+		float average = (float)total / 7;
 		return average;
 	}
 	
